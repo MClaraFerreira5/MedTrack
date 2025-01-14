ad7deec2bb2b1fedc72b6fef48b59c0f222812e1
Ellen Rocha <ellenthaisy52@gmail.com>
2025-01-14 20:29:54 -0300

    Primeiro commit

--- src/main/java/com/medtrack/medtrack/MedtrackApplication.java
+++ src/main/java/com/medtrack/medtrack/MedtrackApplication.java
@@ -1,13 +1,19 @@
 package com.medtrack.medtrack;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedtrackApplication {
+public class MedtrackApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedtrackApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Misericordia");
+
+	}
 }
