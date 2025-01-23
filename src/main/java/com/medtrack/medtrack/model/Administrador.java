e45642fd1da13534cffe2eca916d9a126a8d40fa
yannknnay <kevenyann@gmail.com>
2025-01-23 19:32:58 -0300

    style: Alteracao no campo do telefone

--- src/main/java/com/medtrack/medtrack/model/Administrador.java
+++ src/main/java/com/medtrack/medtrack/model/Administrador.java
@@ -1,12 +1,12 @@
-package com.medtrack.medtrack.model;
-
-
-import com.medtrack.medtrack.model.usuario.Usuario;
-
-import java.time.LocalDate;
-
-public class Administrador extends Usuario {
-    public Administrador(String nome, String email, String nomeUsuario, String senha, Categoria tipoConta, LocalDate dataNascimento) {
-        super(nome, email, nomeUsuario, senha, tipoConta, dataNascimento);
-    }
-}
+//package com.medtrack.medtrack.model;
+//
+//
+//import com.medtrack.medtrack.model.usuario.Usuario;
+//
+//import java.time.LocalDate;
+//
+//public class Administrador extends Usuario {
+//    public Administrador(String nome, String email, String nomeUsuario, String senha, Categoria tipoConta, LocalDate dataNascimento) {
+//        super(nome, email, nomeUsuario, senha, tipoConta, dataNascimento);
+//    }
+//}
