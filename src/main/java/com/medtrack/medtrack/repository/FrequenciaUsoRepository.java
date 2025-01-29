e919b2f114f33f80a9b095d3066018345d14ac25
Ellen Rocha <ellenthaisy52@gmail.com>
2025-01-28 22:06:32 -0300

    add: FrequenciaUsoRepository.
    Signed-off-by: Ellen Rocha <ellenthaisy52@gmail.com>

---
+++ src/main/java/com/medtrack/medtrack/repository/FrequenciaUsoRepository.java
@@ -0,0 +1,9 @@
+package com.medtrack.medtrack.repository;
+
+import com.medtrack.medtrack.model.medicamento.FrequenciaUso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FrequenciaUsoRepository extends JpaRepository<FrequenciaUso, Long> {
+
+}
+
