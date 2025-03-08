a1434af2833bc5ed900c89efd805ebf4ee8d6a4b
YannLeao <yann.kjleao@ufrpe.br>
2025-03-08 20:25:33 -0300

    style: Melhorias no Frontend

--- interface-react/src/Pages/PaginaCadastro/CadastroDependente.jsx
+++ interface-react/src/Pages/PaginaCadastro/CadastroDependente.jsx
@@ -2,24 +2,36 @@ import { useState } from 'react';
 import FormularioCadastro from '../../Componentes/FormularioCadastro';
 
 const CadastroDependente = () => {
-  const [nome, setNome] = useState("");
-  const [numero, setNumero] = useState("");
-  const [idade, setIdade] = useState("");
+  const [formData, setFormData] = useState({
+    nome: "",
+    numero: "",
+    idade: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
   const camposCadastro = [
-    { type: "text", id: "nome-completo", label: "Nome", name: "nome", placeholder: "Digite seu nome", value: nome, onChange: (e) => setNome(e.target.value) },
-    { type: "number", id: "numero", label: "Número", name: "numero", placeholder: "Digite seu número", value: numero, onChange: (e) => setNumero(e.target.value) },
-    { type: "number", id: "idade", label: "Idade", name: "idade", placeholder: "Digite sua idade", value: idade, onChange: (e) => setIdade(e.target.value) }
+    { type: "text", id: "nome-completo", label: "Nome", name: "nome", placeholder: "Digite o nome do dependente" },
+    { type: "number", id: "numero", label: "Número", name: "numero", placeholder: "Digite o número do dependente" },
+    { type: "text", id: "email", label: "Email", name: "email", placeholder: "Digite o email do dependente" }
   ];
 
-  const botaos = [
-    { label: "Criar", destino: "/lista_dependentes"}
-  ]
-  
+  const botaos = [{ label: "Criar", destino: "/lista_dependentes" }];
 
   return (
-    <div className=" h-screen flex justify-center items-center w-full text-center">
-        <FormularioCadastro h1={"Novo Dependente"} campos={camposCadastro} botaos={botaos} />
+      <div className="h-screen flex justify-center items-center w-full text-center">
+        <FormularioCadastro
+            h1="Novo Dependente"
+            campos={camposCadastro}
+            botaos={botaos}
+            formData={formData} // Agora passa o objeto completo
+            handleChange={handleChange} // Passa a função que atualiza o estado
+        />
       </div>
   );
 };
