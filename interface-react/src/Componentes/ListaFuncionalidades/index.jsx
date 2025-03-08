b3ffd2266c111501a4ee5bf9ce0a3487bb29dce6
Ellen Rocha <159654728+EllenRocha1@users.noreply.github.com>
2025-03-08 20:30:09 -0300

    Revert "feat: responsividade da Sidebar"

--- interface-react/src/Componentes/ListaFuncionalidades/index.jsx
+++ interface-react/src/Componentes/ListaFuncionalidades/index.jsx
@@ -1,43 +1,40 @@
 import { Link } from "react-router-dom";
-import { ClipboardList, Box, Bolt, Calendar, Home } from "lucide-react";
+import { ClipboardList, Box, Clock, Calendar, Home } from "lucide-react";
 
 const ListaFuncionalidades = () => {
   return (
-    <nav className="sm:p-2 text-white">
-      <ul className="w-100 flex sm:flex-col space-y-4 sm:justify-start justify-center sm:gap-10 gap-[60px] md:space-y-0">
-        <li className="pt-4">
-          <Link to="/home" className=" flex items-center gap-2 hover:bg-gray-700 rounded">
-            <Home size={30} />
-            <span className="hidden sm:inline ml-2">HOME</span>
+    <nav className="p-4 text-white">
+      <ul className="space-y-4">
+        <li>
+          <Link to="/home" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
+            <Home className="w-5 h-5" /> HOME
           </Link>
         </li>
         <li>
-          <Link to="/lista_dependentes" className="flex items-center gap-2 hover:bg-gray-700 rounded">
-            <ClipboardList size={30} />
-            <span className="hidden sm:inline ml-2">LISTA DE DEPENDENTES</span>
+          <Link to="/lista_dependentes" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
+            <ClipboardList className="w-5 h-5" /> LISTA DE DEPENDENTES
           </Link>
         </li>
         <li>
-          <Link to="/relatorios" className="flex items-center gap-2 hover:bg-gray-700 rounded">
-            <Box size={30}  />
-            <span className="hidden sm:inline ml-2">RELATÓRIOS</span>
+          <Link to="/estoque" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
+            <Box className="w-5 h-5" /> RELATÓRIOS
           </Link>
         </li>
         <li>
-          <Link to="/configuracoes" className="flex items-center gap-2 hover:bg-gray-700 rounded">
-            <Bolt size={30}  />
-            <span className="hidden sm:inline ml-2">CONFIGURAÇÕES</span>
+          <Link to="/lembretes" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
+            <Clock className="w-5 h-5" />CONFIGURAÇÕES
           </Link>
         </li>
         <li>
-          <Link to="/consultas" className="flex items-center gap-2 hover:bg-gray-700 rounded">
-            <Calendar size={30}  />
-            <span className="hidden sm:inline ml-2">LOGOUT</span>
+          <Link to="/consultas" className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded">
+            <Calendar className="w-5 h-5" /> LOGOUT
           </Link>
         </li>
+
       </ul>
     </nav>
   );
 };
 
 export default ListaFuncionalidades;
+
--- interface-react/src/Pages/PerfilDependente/PerfilDependente.jsx
+++ interface-react/src/Pages/PerfilDependente/PerfilDependente.jsx
@@ -13,7 +13,7 @@ const PerfilDependente = () =>{
             <Bell size={40} color="cyan" className="hidden sm:block ml-auto"></Bell>
         </header>
         
-        <main className="md:flex md:w-4/5 gap-5 justify-center mx-auto">
+        <main className="sm:flex 2xl:flex-col md:w-4/5 gap-5 justify-center mx-auto">
             <section className="mx-auto h-screen border border-cyan-300 rounded-lg p-10 ml-3 hidden sm:block ">
                 <Perfil vaiTer={true}/>
             </section>
