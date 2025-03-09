7fb34221044d2a3e80210b402a53ef4797d378b9
YannLeao <yann.kjleao@ufrpe.br>
2025-03-09 17:42:22 -0300

    fix: Retirando o Cross do front

--- src/main/java/com/medtrack/medtrack/controller/AuthController.java
+++ src/main/java/com/medtrack/medtrack/controller/AuthController.java
@@ -29,7 +29,6 @@ public class AuthController {
         this.jwtService = jwtService;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody DadosLogin dados) {
         System.out.println("🔑 Tentativa de login: " + dados.username());
